207cecbf0a316fb93049d41a84565d13214cbe80
Alberto Rovellini <alberto.rovellini@gmail.com>
2022-05-24 16:13:25 -0700

    added code to write ts files

--- AKFIN_to_forcing.R
+++ AKFIN_to_forcing.R
@@ -53,9 +53,10 @@ nmfs_sf <- nmfs_sf %>%
 nmfs_sf %>% ggplot()+geom_sf()+geom_sf_label(aes(label = NMFS_AREA))
 
 area_key <- st_join(atlantis_pts, (nmfs_sf %>% select(NMFS_AREA))) %>%
-  mutate(NMFS_AREA = ifelse(boundary == TRUE, NA, NMFS_AREA))
+  mutate(NMFS_AREA = ifelse(boundary == TRUE, NA, NMFS_AREA)) %>%
+  filter(box_id < 92 | box_id == 94) # keep only boxes within the US
   
-# 1. aggregate catch to Atlantis groups based on key
+# Aggregate species catch to Atlantis groups based on key
 catch_fg <- catch_byf %>% 
   mutate(Species_long = paste(Species.Group, Species)) %>%
   left_join((species_key %>% mutate(Species_long = paste(Species.Group, Species)) %>% select(Species_long, Atlantis_fg)), by = 'Species_long') %>%
@@ -71,7 +72,6 @@ catch_fg <- catch_fg %>% mutate(Month = month(Week.Ending.Date),
 all_fg <- catch_fg %>% pull(Atlantis_fg) %>% unique()
 
 # build a complete time series with all months and NMFS areas
-
 all_years <- unique(year(catch_fg$Month.Year))
 all_months <- 1:12
 all_dates <- list()
@@ -84,50 +84,192 @@ all_dates <- data.frame(expand.grid(all_years, all_months)) %>%
 all_areas <- catch_fg %>% pull(NMFS.Area) %>% unique()
 
 template <- data.frame(expand.grid('Month.Year' = all_dates, 'NMFS.Area' = all_areas))
-# do for one group
 
-this_fg <- 'POL'
-this_name <- atlantis_fg %>% filter(Code == this_fg) %>% pull(Name)
-this_stage <- 'A'
+# Prepare a sequence of all days in the data
+# we will use this to divide monthly values from the data to daily values based on the number of  days
+# we will then only keep the first of those values and make sure we use typeCatchts 1 in force.prm, to limit file size  
+all_dates <- data.frame('Date' = seq(as.Date('1991-01-01'), as.Date('2020-12-31'), by = 'days')) %>%
+  mutate(Year = year(Date), Month = month(Date), Day = day(Date))
 
-this_catch <- catch_fg %>% filter(Atlantis_fg == this_fg) %>% select(-Atlantis_fg)
+# write a function that:
+# 1. Distributes the catch in each NMFS area among Atlantis boxes
+# 2. Converts monthly catch in mt to catch in mg N s-1
+# TODO: it is actually unclear in the manual whether this is mg or mg N. From Isaac's Matlab code and the Get_Imposed_Catch() routine, there are hints that this should be mg N
 
-if((atlantis_fg %>% filter(Code == this_fg) %>% pull(NumStages)) > 1){
-  distribution <- s
-} else {
-  distribution <- s1
+decompose_catch <- function(this_fg, catch_frame, group_frame, s_frame, si_frame, area_frame, days_frame, template_frame){
+  
+  # this_fg <- 'POL'
+  # catch_frame = catch_fg
+  # group_frame = atlantis_fg
+  # s_frame = s
+  # si_frame = s_inv
+  # area_frame = area_key
+  # days_frame = all_dates
+  # template_frame = template
+  
+  print(paste('Doing', this_fg))
+  
+  this_name <- group_frame %>% filter(Code == this_fg) %>% pull(Name)
+  this_stage <- 'A'
+  
+  this_catch <- catch_frame %>% filter(Atlantis_fg == this_fg) %>% select(-Atlantis_fg)
+  
+  if((group_frame %>% filter(Code == this_fg) %>% pull(NumStages)) > 1){
+    distribution <- s_frame
+  } else {
+    distribution <- si_frame
+  }
+  
+  this_dist <- distribution %>% select(contains(paste(this_name, this_stage, sep = '_'))) %>%
+    mutate(box_id = 0:(nrow(.)-1)) %>%
+    select(box_id, contains('S1')) %>%
+    set_names(c('box_id','s'))
+  
+  # join this_catch with the template, and fill the missing values with 0, then join with Atlantis boxes for each NMFS area
+  this_catch_complete <- template_frame %>% 
+    full_join(this_catch, by = c('Month.Year', 'NMFS.Area')) %>%
+    mutate(Catch_mt = coalesce(Catch_mt, 0)) %>% 
+    full_join((area_frame %>% st_set_geometry(NULL)), by = c('NMFS.Area' = 'NMFS_AREA')) 
+  
+  # now join proportions from s by box_id
+  this_catch_box <- this_catch_complete %>% 
+    left_join(this_dist, by = 'box_id') %>%
+    group_by(Month.Year, NMFS.Area) %>%
+    mutate(s_by_NMFS_area = sum(s),
+           corr = s/s_by_NMFS_area) %>% # s/sum of s within a NMFS area
+    ungroup() %>%
+    mutate(Catch_box_mt = Catch_mt * corr) %>% # multiply catch in the NMFS area by the proportion that goes to each box in the NMFS area
+    select(Month.Year, box_id, Catch_box_mt) %>% 
+    complete(Month.Year, box_id) %>% # add boundary boxes
+    mutate(Catch_box_mt = coalesce(Catch_box_mt, 0)) %>% # add 0 catch in boundary boxes
+    filter(!is.na(Month.Year)) %>%
+    mutate(Year = year(Month.Year), Month = month(Month.Year)) # add time columns for later
+  
+  # now break down the catch in time, aiming for daily values
+  daily_catch <- this_catch_box %>% 
+    full_join(days_frame, by = c('Year', 'Month')) %>%
+    group_by(Year, Month, box_id) %>%
+    mutate(Catch_box_day_mt = Catch_box_mt / length(Day), # get catch in mt by day by box
+           Catch_box_day_mgs = Catch_box_day_mt * 1e9 / (60*60*24) / (20 * 5.7)) %>% # convert from mt to mg s-1
+    ungroup() %>%
+    filter(Day == 1) %>% # only keep the first day of each month
+    select(box_id, Date, Catch_box_day_mgs) %>%
+    arrange(box_id, Date) %>%
+    mutate(Code = this_fg)
+  
+  return(daily_catch)
 }
 
-this_dist <- distribution %>% select(contains(paste(this_name, this_stage, sep = '_'))) %>%
-  mutate(box_id = 0:(nrow(.)-1)) %>%
-  select(box_id, contains('S1')) %>%
-  set_names(c('box_id','s'))
+all_catch <- rbindlist(lapply(all_fg, decompose_catch, catch_frame = catch_fg, 
+                     group_frame = atlantis_fg, 
+                     s_frame = s, 
+                     si_frame = s_inv, 
+                     area_frame = area_key,
+                     days_frame = all_dates, 
+                     template_frame = template))
 
-# join this_catch with the template, and fill the missing values with 0
-this_catch_complete <- template %>% full_join(this_catch, by = c('Month.Year', 'NMFS.Area')) %>%
-  mutate(Catch_mt = coalesce(Catch_mt, 0))
+# View --------------------------------------------------------------------
+# make time series plots to visualize this and to compare with original data
 
+all_catch %>%
+  filter(Code == 'COD') %>%
+  mutate(Year = year(Date), Month = month(Date)) %>%
+  full_join(all_dates, by = c('Year', 'Month')) %>%
+  group_by(Year, Month, Code) %>%
+  summarise(Catch_mt = sum(Catch_box_day_mgs * 5.7 * 20 * 60 * 60 * 24 / 1e9)) %>%
+  ungroup() %>%
+  mutate(Date = make_date(year = Year, month = Month)) %>%
+  left_join(atlantis_fg %>% select(Code, Name), by = 'Code') %>%
+  ggplot(aes(x = Date, y = Catch_mt))+
+  geom_line()+
+  theme_bw()+
+  labs(title = 'From catch.ts')
 
-tt <- this_catch_complete %>% full_join((area_key %>% st_set_geometry(NULL)), by = c('NMFS.Area' = 'NMFS_AREA'))
+catch_byf %>%
+  filter(Species.Group == 'Pacific Cod') %>%
+  select(Week.Ending.Date, Catch..mt.) %>%
+  mutate(Year = year(Week.Ending.Date),
+         Month = month(Week.Ending.Date)) %>%
+  group_by(Year, Month) %>%
+  summarise(Catch_mt = sum(Catch..mt.)) %>%
+  ungroup() %>%
+  mutate(Date = make_date(year = Year, month = Month)) %>%
+  ggplot(aes(x = Date, y = Catch_mt))+
+  geom_line()+
+  theme_bw()+
+  labs(title = 'From AKFIN')
 
-# now join proportions from s by box_id
-ttt <- tt %>% full_join(this_dist, by = 'box_id') %>%
-  group_by(Month.Year, NMFS.Area) %>%
-  mutate(Prop_by_NMFS_area = sum(s))
+# make one for plots to show, by year 
+all_catch %>%
+  filter(Code %in% c('COD', 'ATF', 'POL' , 'POP', 'SBF', 'FHS', 'REX', 'FFS', 'FFD', 'RFS', 'RFP', 'RFD', 'THO')) %>%
+  mutate(Year = year(Date), Month = month(Date)) %>%
+  full_join(all_dates, by = c('Year', 'Month')) %>%
+  group_by(Year, Code) %>%
+  summarise(Catch_mt = sum(Catch_box_day_mgs * 5.7 * 20 * 60 * 60 * 24 / 1e9)) %>%
+  ungroup() %>%
+  #mutate(Date = make_date(year = Year, month = Month)) %>%
+  left_join(atlantis_fg %>% select(Code, Name), by = 'Code') %>%
+  ggplot(aes(x = Year, y = Catch_mt, fill = Name))+
+  geom_area()+
+  theme_bw()+
+  labs(x = 'Year', y = 'Catch (mt)')
 
+# Write catch.ts file -----------------------------------------------------
 
+# now write out a ts file for one box
+# however, we will have to add a lot of species here
 
-s1 <- s %>% select(contains(this_fg))
+#remove all existing files first
+all_files <- list.files('../output', full.names = T)
+lapply(all_files, file.remove)
 
-
-s1 <- t(s) %>%
-  data.frame() %>%
-  set_names(0:(ncol(.)-1))
+for(b in 1:length(unique(all_catch$box_id))){
   
-# 4. allocate catch to boxes based on S
-# 5. interpolate from weeks to days
-# 6. convert to appropriate units
-# 7. save
-
-# we will write it out in a separate script - will need a header, organize by box, etc.
-
+  this_box <- unique(all_catch$box_id)[b]
+  
+  t <- all_catch %>% filter(box_id == this_box)
+  # turn date into days from origin
+  origin <- t$Date[1]
+  t <- t %>% 
+    mutate(Time = as.numeric(difftime(Date, origin, units = 'days'))) %>%
+    select(Time, Code, Catch_box_day_mgs)
+  
+  # expand to all functional groups
+  # make a template
+  tmp <- expand.grid('Time' = unique(t$Time), 'Code' = unique(atlantis_fg$Code))
+  # join it to the data
+  tt <- tmp %>% 
+    left_join(t, by = c('Time', 'Code')) %>%
+    mutate(Catch_box_day_mgs = coalesce(Catch_box_day_mgs, 0))
+  
+  # now have it as horizontal table
+  ttt <- tt %>% pivot_wider(names_from = Code, values_from = Catch_box_day_mgs)
+  
+  # make header
+  header_file <- paste0('../output/catch', this_box, '.ts')
+  
+  cat(paste("# Historical catch time series file for Atlantis GOA box", this_box, ", years 1991-2020\n", sep = " "), file = header_file, append = T)
+  cat("#\n", file = header_file, append = T)
+  cat(paste0("## COLUMNS ", ncol(ttt), "\n"), file = header_file, append = T)
+  cat("##\n", file = header_file, append = T)
+  
+  # write time column
+  cat("## COLUMN1.name Time\n", file = header_file, append = T)
+  cat("## COLUMN1.long_name Time\n", file = header_file, append = T)
+  cat("## COLUMN1.units days since 1991-01-01 12:00:00\n", file = header_file, append = T)
+  cat("## COLUMN1.missing_value 0\n", file = header_file, append = T)
+  cat("##\n", file = header_file, append = T)
+  
+  all_fg <- atlantis_fg$Code
+  
+  # write column for each group
+  for(i in 1:length(all_fg)){
+    cat(paste0("## COLUMN", i+1, ".name ", all_fg[i], "\n"), file = header_file, append = T)
+    cat(paste0("## COLUMN", i+1, ".long_name ", all_fg[i], "\n"), file = header_file, append = T)
+    cat(paste0("## COLUMN", i+1, ".units mg s-1\n"), file = header_file, append = T)
+    cat(paste0("## COLUMN", i+1, ".missing_value 0\n"), file = header_file, append = T)
+    cat("##\n", file = header_file, append = T)
+  }
+  
+  write.table(ttt, file = header_file, append = T, sep = " ", row.names = FALSE, col.names = FALSE)
+}
