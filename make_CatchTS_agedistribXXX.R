6d5b05317c5de24e10f77940fd489bd06000fd98
Alberto Rovellini <alberto.rovellini@gmail.com>
2022-08-25 09:47:28 -0700

    Adding code to construct catch at age vectors for catch.ts

---
+++ make_CatchTS_agedistribXXX.R
@@ -0,0 +1,6 @@
+# Alberto Rovellini
+# August 25 2022
+# Convert selectivity patterns to Atlantis parameter CatchTS_agedistribXXX vectors 
+# This is for Tier 3 groups and it is based on selectivity from stock assessment models
+
+
